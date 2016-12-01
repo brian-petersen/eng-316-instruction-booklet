58e9da35681382bbbbeb0e5083a029913e781978
Brian Petersen <skiwalk3r@gmail.com>
2016-11-30 22:18:41 -0700

    help

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    hljs.initHighlightingOnLoad();
+    hljs.initHighlighting();
 
     // $('pre code').click(function() {
     //     var refNode = jQuery( this )[0];
